b38993899723074afaf7aff2db4eabe7764bb563
linnjoha <linn.wallin@hotmail.com>
2024-02-18 13:42:58 +0100

    git ignore

--- src/Components/TextInput.tsx
+++ src/Components/TextInput.tsx
@@ -1,3 +1,15 @@
+type InputValues = {
+  cardnumber: number;
+  cardholderName: string;
+  validThru: number;
+  ccv: number;
+  vendor: {
+    img: string;
+    vendorName: string;
+    backgroundColor: string;
+  };
+};
+
 const TextInput = () => {
   return <div>TextInput</div>;
 };
