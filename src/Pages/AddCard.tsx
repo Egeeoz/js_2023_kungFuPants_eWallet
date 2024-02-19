5359fd4688034e019b714bb56f83391206b9e538
Tobias Andersson <tobiaserikandersson@gmail.com>
2024-02-19 12:25:11 +0200

    button links

--- src/Pages/AddCard.tsx
+++ src/Pages/AddCard.tsx
@@ -3,16 +3,21 @@ import Top from "../Components/Top";
 import Card from "../Components/Card";
 import TextInput from "../Components/TextInput";
 import DropDownInput from "../Components/DropDownInput";
+import ButtonLinks from "../constants/ButtonLinks";
 
 const AddCard = () => {
+
+  const addButton = ButtonLinks.find((link) => link.key === "addButton")
+
   return (
     <>
       <Top text="Add a new bank card" smalltext="New Card" />
       <Card />
       <TextInput />
       <DropDownInput />
-
-      <Button text="Add card" to={"/"} className="addCardBtn"></Button>
+      {addButton && (
+        <Button key={addButton.key} text={addButton.text} to={addButton.to}/>
+      )}
     </>
   );
 };
