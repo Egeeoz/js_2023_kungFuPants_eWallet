d3b2596c9d10c7c92f007026e84715f3d1c31738
Egeeoz <ozakdemirege@hotmail.com>
2024-02-20 16:14:21 +0100

    Added delete icon component and used it in top in home page

---
+++ src/Components/DeleteIcon.tsx
@@ -0,0 +1,7 @@
+import "../abstracts/deleteIcon.scss";
+
+const DeleteIcon = () => {
+  return <a className="deleteIcon" href=""></a>;
+};
+
+export default DeleteIcon;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,20 +1,20 @@
 import Button from "../Components/Button";
 import Top from "../Components/Top";
-import Card from "../Components/Card"
+import Card from "../Components/Card";
 import CardStack from "../Components/CardStack";
 import ButtonLinks from "../constants/ButtonLinks";
+import DeleteIcon from "../Components/DeleteIcon";
 
 const Home = () => {
-
-  const addButton = ButtonLinks.find((link) => link.key === "addNewButton")
+  const addButton = ButtonLinks.find((link) => link.key === "addNewButton");
 
   return (
     <>
-      <Top text="E-wallet" smalltext="Active card" />
-      <Card/>
-      <CardStack/>
+      <Top text="E-wallet" smalltext="Active card" icon={<DeleteIcon />} />
+      <Card />
+      <CardStack />
       {addButton && (
-        <Button key={addButton.key} text={addButton.text} to={addButton.to}/>
+        <Button key={addButton.key} text={addButton.text} to={addButton.to} />
       )}
     </>
   );
