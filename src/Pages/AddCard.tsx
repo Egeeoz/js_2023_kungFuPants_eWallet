aa930f5a17fa10992e2d0623a0b227fad127299e
Ahmed Abbas <ahmed_abbas87@hotmail.com>
2024-02-20 14:09:35 +0100

    add Home link

--- src/Pages/AddCard.tsx
+++ src/Pages/AddCard.tsx
@@ -3,12 +3,16 @@ import Top from "../Components/Top";
 import Card from "../Components/Card";
 import TextInput from "../Components/TextInput";
 import ButtonLinks from "../constants/ButtonLinks";
-
+import { Link } from "react-router-dom";
+import "../abstracts/linkBtn.scss";
 const AddCard = () => {
   const addButton = ButtonLinks.find((link) => link.key === "addButton");
 
   return (
     <>
+      <Link className="linkBtn" to="/">
+        Home
+      </Link>
       <Top text="Add a new bank card" smalltext="New Card" />
       <Card />
       <TextInput />
