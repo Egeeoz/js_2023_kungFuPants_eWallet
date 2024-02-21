f863ab5d89a7ed44a299f0b01bf61fbc1441bf3d
linnjoha <linn.wallin@hotmail.com>
2024-02-21 19:30:50 +0100

    defaultstyling

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -5,31 +5,32 @@ import chipLogo from "../assets/chip.png";
 import "../abstracts/card.scss";
 import { CardProps } from "../models/CardProps";
 
-
-
-const CardList: React.FC<CardProps> = ({ cardNumber, cardHolderName, validThru, vendor }) => {
-
- 
-    return (
-      <div className={`card ${vendor}`}>
-        <div className="logoRow">
-          <img className="wifiLogo" src={wifiLogo} alt="Wifi Logo" />
-          <img className="chipLogo" src={chipLogo} alt="Chip Logo" />
-          <div className={`cardLogo ${vendor}`} />
-        </div>
-        <div className={`cardNo ${vendor}`}>
-          <p className={`cardNumber ${vendor}`}>{cardNumber}</p>
-        </div>
-        <div className="titleRow">
-          <p className={`titleText ${vendor}`}>CARDHOLDER NAME</p>
-          <p className={`titleText ${vendor}`}>VALID THRU</p>
-        </div>
-        <div className="infoRow">
-          <p className={`holderName ${vendor}`}>{cardHolderName}</p>
-          <p className={`validThru ${vendor}`}>{validThru}</p>
-        </div>
+const CardList: React.FC<CardProps> = ({
+  cardNumber,
+  cardHolderName,
+  validThru,
+  vendor,
+}) => {
+  return (
+    <div className={`card ${vendor}`}>
+      <div className="logoRow">
+        <img className="wifiLogo" src={wifiLogo} alt="Wifi Logo" />
+        <img className="chipLogo" src={chipLogo} alt="Chip Logo" />
+        <div className={`cardLogo ${vendor}`} />
       </div>
-    );
+      <div className={`cardNo ${vendor}`}>
+        <p className={`cardNumber ${vendor}`}>{cardNumber}</p>
+      </div>
+      <div className="titleRow">
+        <p className={`titleText ${vendor}`}>CARDHOLDER NAME</p>
+        <p className={`titleText ${vendor}`}>VALID THRU</p>
+      </div>
+      <div className="infoRow">
+        <p className={`holderName ${vendor}`}>{cardHolderName}</p>
+        <p className={`validThru ${vendor}`}>{validThru}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CardList;
--- src/Pages/AddCard.tsx
+++ src/Pages/AddCard.tsx
@@ -2,6 +2,16 @@ import Top from "../Components/Top";
 import TextInput from "../Components/TextInput";
 import { Link } from "react-router-dom";
 import "../abstracts/linkBtn.scss";
+import Card from "../Components/Card";
+import { CardProps } from "../models/CardProps";
+
+const deafaultCard: CardProps = {
+  cardNumber: "XXXX XXXX XXXX XXXX",
+  cardHolderName: "FIRSTNAME LASTNAME",
+  validThru: "MM/YY",
+  vendor: "default",
+  active: false,
+};
 
 const AddCard = () => {
   return (
@@ -10,7 +20,7 @@ const AddCard = () => {
         Home
       </Link>
       <Top text="Add a new bank card" smalltext="New Card" />
-      {/* <Card/> */}
+      <Card {...deafaultCard} />
       <TextInput />
     </>
   );
